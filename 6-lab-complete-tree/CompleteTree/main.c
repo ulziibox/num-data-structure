5f038db7f8111d5bdc8d4ca9753da8c99ffde5da
Bat-Ulzii Batbayar <ulziibox@gmail.com>
2020-12-11 18:02:09 +0800

    Lab 6 - Complete Tree done with comments

--- 6-lab-complete-tree/CompleteTree/main.c
+++ 6-lab-complete-tree/CompleteTree/main.c
@@ -5,25 +5,28 @@ int main()
 {
 	/* List үүсгэн, эхлүүлэх */
 	struct Tree st;
-	int n=20;
+	int n = 20;
 	init(&st, n);
 	int t, x, pos;
-	int a[20]={	1,
-				11,12,
-				111,112,121,122,
-				1111,1112,1121,1122,1211,1212,1221,1222,
-				11111,11112,11121,11122,11211};
-	for(int i=0;i<n;i++){
-        add(&st,a[i]);
+	int a[20] = {1,
+				 11, 12,
+				 111, 112, 121, 122,
+				 1111, 1112, 1121, 1122, 1211, 1212, 1221, 1222,
+				 11111, 11112, 11121, 11122, 11211};
+	for (int i = 0; i < n; i++)
+	{
+		add(&st, a[i]);
 	}
-	while (1) {
+	while (1)
+	{
 		printf("1: search, 2: child of, 3: parent of, 4: sibling of"
-		       "5: ancestor of, 6: desendant of, 7: level of, 8: height of sub tree, 9: pre order,"
+			   "5: ancestor of, 6: desendant of, 7: level of, 8: height of sub tree, 9: pre order,"
 			   "10: post order, 11: in order, 12: leafs, 13: exit\n");
 		scanf("%d", &t);
 		error = 0;
 
-		switch (t) {
+		switch (t)
+		{
 		case 1:
 			printf("Xaix toog oruul: ");
 			scanf("%d", &x);
@@ -40,7 +43,7 @@ int main()
 			if (pos == -1)
 				printf("%d utga oldsongui\n", x);
 			else
-				parent(&st,pos);
+				parent(&st, pos);
 			break;
 		case 3:
 			printf("Xaix toog oruul: ");
@@ -49,71 +52,71 @@ int main()
 			if (pos == -1)
 				printf("%d utga oldsongui\n", x);
 			else
-				children(&st,pos);
+				children(&st, pos);
 			break;
-        case 4:
+		case 4:
 			printf("Xaix toog oruul: ");
 			scanf("%d", &x);
 			pos = find(&st, x);
 			if (pos == -1)
 				printf("%d utga oldsongui\n", x);
 			else
-				{
-				    int s=sibling(&st,pos);
-                    if(s!=-1)
-                        printf("%d utgatai zangilaanii ah duu n %d",x,st.dat[s]);
-                    else
-                        printf("%d utgatai zangilaa ah duugui %d",x);
-				}
+			{
+				int s = sibling(&st, pos);
+				if (s != -1)
+					printf("%d utgatai zangilaanii ah duu n %d\n", x, st.dat[s]);
+				else
+					printf("%d utgatai zangilaa ah duugui\n", x);
+			}
 			break;
 
-        case 5:
+		case 5:
 			printf("Xaix toog oruul: ");
 			scanf("%d", &x);
 			pos = find(&st, x);
 			if (pos == -1)
 				printf("%d utga oldsongui\n", x);
 			else
-				ancestors(&st,pos);
+				ancestors(&st, pos);
 			break;
-        case 6:
+		case 6:
 			printf("Xaix toog oruul: ");
 			scanf("%d", &x);
 			pos = find(&st, x);
 			if (pos == -1)
 				printf("%d utga oldsongui\n", x);
 			else
-				descendants(&st,pos);
+				descendants(&st, pos);
 			break;
-        case 7:
+		case 7:
 			printf("Xaix toog oruul: ");
 			scanf("%d", &x);
 			pos = find(&st, x);
 			if (pos == -1)
 				printf("%d utga oldsongui\n", x);
 			else
-				level(&st,pos);
+				level(&st, pos);
 			break;
-        case 8:
+		case 8:
 			printf("Xaix toog oruul: ");
 			scanf("%d", &x);
 			pos = find(&st, x);
 			if (pos == -1)
 				printf("%d utga oldsongui\n", x);
 			else
-				height(&st,pos);
+				height(&st, pos);
 			break;
-        case 9:
-			preorder(&st,0);
+		case 9:
+			preorder(&st, 0);
 			break;
-        case 10:
-			postorder(&st,0);
+		case 10:
+			postorder(&st, 0);
 			break;
-        case 11:
-			inorder(&st,0);
+		case 11:
+			inorder(&st, 0);
 			break;
-        case 12:
-			leaves(&st,0);
+		case 12:
+			leaves(&st, 0);
 			break;
 		default:
 			exit(0);
